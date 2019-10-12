31dc1e8622ff86c7114124ec013e3cabb74cf5e3
Valiantsin Danilau <valentine.danilov.ur@gmail.com>
2019-10-12 13:41:40 +0300

    Some fixes made

--- src/main/java/by/danilov/controller/SampleController.java
+++ src/main/java/by/danilov/controller/SampleController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -25,14 +26,14 @@ public class SampleController {
                     /*Value equals to input name on form*/
                     /*Also you can map params from form without @RequestParam */
                     /*In this case you just have to set method parameter name to input name from form*/
-                    value = "number") Integer inputNumber,
+                    "number") Integer asd,
             /*Object to save request params after redirect*/RedirectAttributes redirectAttributes) {
 
-        System.out.println(inputNumber);
+        System.out.println(asd);
 
         /*As after redirect we lose all request params we need to pass RedirectAttributes to controller method*/
         /*This object saves all attributes that you put into it using addFlashAttributes method between redirects*/
-        redirectAttributes.addFlashAttribute("result", "Here is result");
+        redirectAttributes.addFlashAttribute("result", "Here is passed param: " + asd);
 
         /*It is good practice to redirect after submitting using POST*/
         return "redirect:/form";
